ffccb0bc3af0b2f553c6420a629ed30043099bc7
Kuncheng <852444214@qq.com>
2019-05-31 02:36:40 -0700

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    }
-  }
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,68 +1,78 @@
-import React, { Component } from "react";
-import {
-    Card,
-    CardImg,
-    CardText,
-    CardBody,
-    CardTitle,
-    ListGroup,
-    ListGroupItem
-} from "reactstrap";
-class DishDetail extends Component {
-    
-    render() {
-        const renderComments = comments => {
-            if (comments != null) {
-                return (
-                    <div className="container">
-                        <h4>Comments</h4>
-                        <ListGroup>
-                            {comments.comments.map(comment => {
-                                return (
-                                    <ListGroupItem key={comment.id}>
-                                        <div>{comment.comment}</div>
-                                        <div>
-                                            --
-                      {comment.author},{new Intl.DateTimeFormat('en-US',{year:'numeric', 'month':'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                        </div>
-                                    </ListGroupItem>
-                                );
-                            })}
-                        </ListGroup>
-                    </div>
-                );
-            } else {
-                return <div />;
-            }
-        };
-        const renderDish = dish => {
-            if (dish != null) {
-                return (
-                    <div>
-                        <Card>
-                            <CardImg
-                                top
-                                src={this.props.selectedDish.image}
-                                alt={this.props.selectedDish.name}
-                            />
-                            <CardBody>
-                                <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                                <CardText>{this.props.selectedDish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                );
-            } else {
-                return <div />;
-            }
-        };
+import React from "react";
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, ListGroup,ListGroupItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function RenderDish({ dish }) {
+    if (dish != null) {
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">{renderDish(this.props.selectedDish)}</div>
-                <div className="col-12 col-md-5 m-1">
-                    {renderComments(this.props.selectedDish)}
+                <Card>
+                    <CardImg
+                        top
+                        src={dish.image}
+                        alt={dish.name}
+                    />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+        );
+    } else {
+        return <div />;
+    }
+};
+function RenderComments({ comments }) {
+    if (comments != null) {
+        return (
+            <div>
+                <h4>Comments</h4>
+                <div>
+                    {comments.map(comment => {
+                        return (
+                            <div key={comment.id}>
+                                <div>{comment.comment}</div>
+                                <div>
+                                    --
+                      {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', 'month': 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
+        );
+    } else {
+        return <div />;
+    }
+};
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
+        );
+    } else {
+        return (
+            <div></div>
         );
     }
 }
